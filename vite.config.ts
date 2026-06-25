6d6471461136146c8fff296c227e93aff5c20833
drixian <business@drixian.org>
2026-06-25 16:45:33 -0400

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// If your repository is named exactly "drixian.github.io", leave base as '/'
-// If your repository is a subfolder like "github.com/yourname/drixian", change base to '/drixian/'
-export default defineConfig({
-  plugins: [react()],
-  base: '/', 
-});
